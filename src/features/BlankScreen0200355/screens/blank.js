4b2189c0bb786eeb1bf9e2b56e1c8c37eef66ca1
Crowdbotics <team@crowdbotics.com>
2021-02-04 00:22:35 +0000

    Update components

--- src/features/BlankScreen0200355/screens/blank.js
+++ src/features/BlankScreen0200355/screens/blank.js
@@ -21,20 +21,50 @@ import { SlideMenuIcon } from "../../../navigator/slideMenuIcon"
 class Blank extends React.Component {
   constructor(props) {
     super(props)
+
     this.state = {}
   }
   static navigationOptions = ({ navigation }) => {
     return { headerLeft: <SlideMenuIcon navigationProps={navigation} /> }
   }
 
-  render = () => <View />
+  render = () => (
+    <ImageBackground
+      source={{
+        uri:
+          "https://d3tklmlrp1a8c2.cloudfront.net/media/resources/project/24262/593e08b7-51d8-483f-9c1c-e3254c6d3564.JPEG"
+      }}
+      style={styles.ImageBackground_1}
+    />
+  )
 }
 
-const styles = StyleSheet.create({ View_1: {} })
+const styles = StyleSheet.create({
+  ImageBackground_1: {
+    width: 1,
+    height: 1,
+    marginLeft: 1,
+    marginRight: 1,
+    marginTop: 1,
+    marginBottom: 1,
+    paddingLeft: 1,
+    paddingRight: 1,
+    paddingTop: 1,
+    paddingBottom: 1,
+    borderWidth: 1,
+    borderLeftWidth: 1,
+    borderRightWidth: 1,
+    borderTopWidth: 1,
+    borderBottomWidth: 1,
+    borderRadius: 1
+  }
+})
 const mapStateToProps = state => {
   return {}
 }
+
 const mapDispatchToProps = () => {
   return {}
 }
+
 export default connect(mapStateToProps, mapDispatchToProps)(Blank)
